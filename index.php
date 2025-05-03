5781a6f4d9e0050ae0f8b804211910e7acb5aecd
brendosousa <brendo.n.s.sousa@gmail.com>
2025-05-02 21:53:15 -0300

    feat: Alteração no html e nas páginas para incluir mais informações

--- index.php
+++ index.php
@@ -1,68 +1,55 @@
 <?php
-
-    $sinopse_morte_e_vida_de_osmar_fragoso = "Esse livro trata da história de um jovem que aspira ser cineasta. Ele trabalha como repositor de mercadorias de um supermercado numa pequena cidade do interior nordestino. Fascinado pelo universo da sétima arte, o rapaz desenvolveu várias ideias de roteiros que espera um dia transformar em filmes de grande sucesso. 
-Certo dia, ele descobre que haverá um festival de cinema em São Paulo e, nesse evento, haverá um concurso de filmes amadores sendo que um dos jurados da competição será um dos seus cineastas favoritos, Alfredo Santana.
-Renan decide entrar na competição e, mesmo sem recurso algum, começa uma longa batalha desde reunir um time para sua produção até conseguir os recursos necesssários.
-Para garantir seu triunfo no festival, ele decide usar seu melhor roteiro: A Morte e a Vida de Osmar Fragoso. Esse roteiro conta a história de um homem que morre e, no céu, faz um acordo com Deus para voltar no tempo e corrigir todos os seus erros de sua vida a fim de provar que todas as pessoas seriam salvas se tivessem uma segunda chance de fazer tudo certo.;
-Para realizar tal feito, Renan decide também adotar um nome artístico: Renan Marcovski, para atrair mais atenção.";
-
-    $sinopose_chico_barauna = "Um humilde dono de mercearia numa pequena cidade nordestina se vê numa situação em que precisa assumir a liderança do município para evitar que pessoas poderosas destruam o seu lar e de todo o seu povo.";
-
-    $sinopse_fazenda_treze = "Um jovem rapaz descobre que é o herdeiro de uma fazenda em uma pequena cidade do interior. Diante disso, ele decide aderir à vida de homem do campo enquanto descobre essa cidade fantástica em que vai viver.";
-
-    $sinopse_marra_vs_leone = "Dois super gênios empreendedores da informática chegam ao Brasil com a promessa de transformar o país numa utopia tecnológica. Mas será que algum deles irá conseguir?";
-
-    $sinopse_ceifador = "Brasil dos anos 80, pós ditadura, a democracia começa a entrar nos eixos. Porém, em cidades menores, especialmente do interior do Nordeste, o coronelismo ainda impera. Grandes fazendeiros, seus parentes e donos de grandes terras dominam e controlam os aspectos social, político e econômico dessas localidades.
-Diante esse cenário, uma figura misteriosa de preto, mascarada, encapuzada e em posse de uma foice surge e começa a eliminar cada um desses magnatas que abusam do poder para impor suas vontades.";
-
-    $sinopse_homem_que_vendeu_o_mundo = "Emílio Pimenta é um morador de uma zona rural no interior do Brasil. Sua vida muda de ponta a cabeça quando uma nave alienígena pousa em seu quintal e os visitantes extraterrestres informam que procuram o líder do planeta para negociar uma venda.
-Emílio se passa por 'presidente' do mundo e aceita vender a Terra em troca de se tornar o governante supremo de um planeta chamado Askar.";
-
-    $sinopse_especialistas = "Cinco desempregados sem nada a perder se unem para destruir a empresa que ferrou com suas vidas.";
-
-    $sinpose_por_que_as_coisas_caem = "Amanda é uma adolescente com um jeito 'desastroso' de lidar com coisas, situações e pessoas. Isso a leva a confrontar sua própria personalidade e assumir uma nova atitude frente a uma viagem para apresentar um projeto que vem realizando na escola num evento nacional.
-Junto a Kenya, sua melhor amiga, Amanda vai descobrir do jeito mais 'estabanado' possível que você não pode ser ninguém além de você mesmo.";
-
-    $sinopose_vine = "Dois irmãos, Doni e Jean, certo dia recebem a visita de sua prima Paula e, em meio à conversas despretensiosas, ela menciona que tem uma colega que possui o mesmo gosto musical incomum de Jean e o encoraja a conhecê-la e fazer um primeiro contato pelas redes sociais.
-Doni, sem nenhuma pretensão, adiciona a mesma garota em suas redes sociais e logo esta inicia uma conversa. Após vários papos, os dois se encantam e iniciam um relacionamento, porém é complicado pois a garota, Vine, possui grandes problemas de auto-estima e também problemas para exprimir seus sentimentos.
-Após um tempo, Doni decide deixar Vine, percebendo que o relacionamento não iria para frente. Nisso, Jean se aproxima mais da garota e logo inicia um relacionamento com ela.
-Quando descobre isso, Doni se arrepende e tenta voltar com Vine enquanto a garota se vê numa situação em que não sabe o que fazer.";
+ include "info.php";
 
 if (isset($_POST['submit'])) {
     $obra_escolhida = $_POST['operation'];
     switch ($obra_escolhida) {
         case "1":
             $result = $sinopse_morte_e_vida_de_osmar_fragoso;
+            $pos = 0;
             break;;
         case "2":
             $result = $sinopose_chico_barauna;
+            $pos = 1;
             break;
         case "3":
             $result = $sinopse_fazenda_treze;
+            $pos = 2;
             break;
         case "4":
             $result = $sinopse_marra_vs_leone;
+            $pos = 3;
             break;
         case "5":
             $result = $sinopse_ceifador;
+            $pos = 4;
             break;
         case "6":
             $result = $sinopse_homem_que_vendeu_o_mundo;
+            $pos = 5;
             break;
         case "7":
             $result = $sinopse_especialistas;
+            $pos = 6;
             break;
         case "8":
             $result = $sinpose_por_que_as_coisas_caem;
+            $pos = 7;
             break;                    
         case "9":
             $result = $sinopose_vine;
+            $pos = 8;
             break; 
         default:
             $result = "Escolha inválida.";
             break;
     }
-    echo "Sinopse do livro \n: $result";
+    echo "
+        <h2>$titulos[$pos]</h2>
+        <div>
+            $result    
+        </div>";
+    
 }
 
 
