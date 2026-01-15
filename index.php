fc0d61a3d4792cb4f6de2908b005b54a938b6961
Brendo Sousa <brendo.n.s.sousa@gmail.com>
2026-01-15 20:23:56 -0300

    Update synopsis assignments in switch cases

--- index.php
+++ index.php
@@ -8,40 +8,36 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['submit'])) {
 
     switch ($obra_escolhida) {
         case "1":
-            $result = $sinopse_morte_e_vida_de_osmar_fragoso;
+            $result = $sinopose_chico_barauna;
             $pos = 0;
             break;
         case "2":
-            $result = $sinopose_chico_barauna;
+            $result = $sinopse_fazenda_treze;
             $pos = 1;
             break;
         case "3":
-            $result = $sinopse_fazenda_treze;
+            $result = $sinopse_marra_vs_leone;
             $pos = 2;
             break;
         case "4":
-            $result = $sinopse_marra_vs_leone;
+            $result = $sinopse_ceifador;
             $pos = 3;
             break;
         case "5":
-            $result = $sinopse_ceifador;
+            $result = $sinopse_homem_que_vendeu_o_mundo;
             $pos = 4;
             break;
         case "6":
-            $result = $sinopse_homem_que_vendeu_o_mundo;
+            $result = $sinopse_especialistas;
             $pos = 5;
             break;
         case "7":
-            $result = $sinopse_especialistas;
-            $pos = 6;
-            break;
-        case "8":
             $result = $sinpose_por_que_as_coisas_caem;
-            $pos = 7;
+            $pos = 6;
             break;                    
-        case "9":
+        case "8":
             $result = $sinopose_vine;
-            $pos = 8;
+            $pos = 7;
             break; 
         default:
             $result = "Escolha inválida.";
